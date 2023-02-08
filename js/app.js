254535d9f150ee7b313d215ce9e0b66a26535ab1
GraceGenuardi <graziagenuardi96@gmail.com>
2023-02-08 14:10:53 +0100

    EDIT struttura JS

---
+++ js/app.js
@@ -0,0 +1,31 @@
+console.log ('Palidroma')
+
+//****PALIDROMA****/
+
+//1.Chiedere all’utente di inserire una parola
+
+
+
+//2.Creare una funzione per capire se la parola inserita è palindroma
+
+
+//3.Comunicare all’utente  se la parola che ha inserito è palindroma oppure no
+
+
+
+/******PARI E DISPARI*******/
+
+//1.L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
+
+
+
+//2.Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+
+
+//3.Sommiamo i due numeri
+
+
+//4.Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
+
+
+//5.Comunichiamo all’utente chi ha vinto.
